8036eeb34d54cce68479a5b14f219d6b88ab1ebb
KaitoHH <hh.kaito@gmail.com>
2018-03-11 14:03:01 +0800

    add method dispatcher, url mapping decorator

--- resource/ResourceBase.py
+++ resource/ResourceBase.py
@@ -1,5 +1,7 @@
 from flask_peewee.rest import RestResource
 from auth import auth
+from flask import request
+from functools import wraps
 
 
 class AuthorizedRestResource(RestResource):
@@ -9,3 +11,50 @@ class AuthorizedRestResource(RestResource):
 
     def authorized_urls(self):
         raise NotImplemented
+
+
+class BaseRestResource(RestResource):
+    url_manager = {}
+
+    @classmethod
+    def route(cls, url, method):
+        def decorator(func):
+            method_dict = cls.url_manager.setdefault(url, {})
+            method_dict[method] = func
+
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                return func(*args, **kwargs)
+
+            return wrapper
+
+        return decorator
+
+    def method_dispatcher(self, get=None, post=None, put=None, delete=None):
+        dispatch = {'GET': get, 'POST': post, 'PUT': put, 'DELETE': delete}
+
+        def wrapper(*args, **kwargs):
+            return dispatch[request.method](self, *args, **kwargs)
+
+        return wrapper
+
+    @staticmethod
+    def rename(new_name):
+        def decorator(func):
+            func.__name__ = new_name
+            return func
+
+        return decorator
+
+    def get_urls(self):
+        urls = []
+        for key, value in self.url_manager.items():
+            f = self.method_dispatcher(
+                value.get('GET', None),
+                value.get('POST', None),
+                value.get('PUT', None),
+                value.get('DELETE', None)
+            )
+            f = self.rename(key)(f)
+            urls.append((key, self.require_method(f, list(value.keys()))))
+        return urls
--- resource/UserResource.py
+++ resource/UserResource.py
@@ -1,23 +1,17 @@
-from flask_peewee.rest import RestResource
-
+from .ResourceBase import BaseRestResource as Rest
 from auth import auth
 from models import User
 from api import api
 from flask import request
 
 
-class UserResource(RestResource):
+class UserResource(Rest):
     exclude = ('password', 'email',)
 
     def get_api_name(self):
         return 'user'
 
-    def get_urls(self):
-        return [
-            ('/login', self.login),
-            ('/logout', self.logout)
-        ]
-
+    @Rest.route('/login', 'POST')
     def login(self):
         username = request.form['username']
         password = request.form['password']
@@ -26,10 +20,18 @@ class UserResource(RestResource):
             auth.login_user(user)
             return self.response({'status': 'login success', 'ret_code': 0})
         else:
-            return self.response({'status': 'xxx', 'ret_code': 101})
+            return self.response({'status': 'incorrect username or password', 'ret_code': 101})
 
     def logout(self):
         pass
+
+    @Rest.route('/r', 'POST')
+    def r1(self):
+        return self.response({'post': 'post'})
+
+    @Rest.route('/r', 'GET')
+    def r2(self):
+        return self.response({'get': 'get'})
 
 
 api.register(User, UserResource)
