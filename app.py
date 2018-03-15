2b4da076607b91da461b5f1985d673c6cfe8133f
KaitoHH <hh.kaito@gmail.com>
2018-03-15 19:06:49 +0800

    enable CORS

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_peewee.db import Database
 
 app = Flask(__name__)
 app.config.from_json('config.json')
+cors = CORS(app)
 
 db = Database(app)
