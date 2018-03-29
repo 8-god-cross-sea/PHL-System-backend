ea0d20a1d0a32beb4a29e678f5d271a06f16cc32
KaitoHH <hh.kaito@gmail.com>
2018-03-29 17:38:41 +0800

    revert CORS

--- app/config.py
+++ app/config.py
@@ -8,6 +8,7 @@ class BaseConfig:
     """
     DEBUG = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'a default secret key')
+    CORS_SUPPORTS_CREDENTIALS = True
 
 
 class DevelopmentConfig(BaseConfig):
