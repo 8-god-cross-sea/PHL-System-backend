cf0e35b9a1c651328e32482cb02d5b320aadb23a
KaitoHH <hh.kaito@gmail.com>
2018-04-19 15:57:51 +0800

    add exam api

--- app/api/resources/exam_resource.py
+++ app/api/resources/exam_resource.py
@@ -3,27 +3,50 @@ from app.api.auth.role_auth import RoleAuth
 from flask_peewee.utils import get_object_or_404
 from app import auth
 from flask import request
-from app.model.user import User
-from app.model.exam import Exam
+from playhouse.shortcuts import model_to_dict
+from app.model.choice import Choice
+from flask_peewee.utils import make_password, check_password
+from app.utils.response_manager import NOT_PERMITTED_RESPONSE
 
 
 class ExamResource(APIRestResource):
     default_access = RoleAuth.ADMIN
-    show_m2m = True
-    show_backref = True
+    access_dict = {
+        'my_exams': RoleAuth.ANY_USER,
+        'begin_exam': RoleAuth.ANY_USER
+    }
 
     def get_urls(self):
         return super().get_urls() + (
             ('/begin', self.require_method(self.begin_exam, ['GET'])),
+            ('/my', self.require_method(self.my_exams, ['GET'])),
         )
+
+    def make_token(self, exam):
+        return make_password(str(exam.id) + str(exam.token))
+
+    def check_token(self, exam, token):
+        return check_password(str(exam.id) + str(exam.token), token)
+
+    def my_exams(self):
+        user = auth.get_logged_in_user()
+        # user = User.get_by_id(2)
+        return self.response({"objects": [model_to_dict(exam, exclude=self.always_exclude) for exam in user.exams]})
 
     def begin_exam(self):
         eid = request.args.get('eid')
         user = auth.get_logged_in_user()
+        # user = User.get_by_id(3)
         exam = get_object_or_404(self.get_query(), self.pk == eid)
         print(eid)
         print(exam.users)
         if user in exam.users:
-            return self.response({'1'})
+            choices = exam.test_paper.choices
+            response = {
+                'problem': [model_to_dict(choice, exclude=(Choice.answer, Choice.case_type, Choice.id)) for choice in
+                            choices],
+                'token': self.make_token(exam)
+            }
+            return self.response(response)
         else:
-            return self.response({'0'})
+            return NOT_PERMITTED_RESPONSE
