960184eecf7f2865066cece321c6a93389a22fce
KaitoHH <hh.kaito@gmail.com>
2018-04-22 14:03:35 +0800

    bug fix in vaccine unit test

--- tests/vaccine_api_test.py
+++ tests/vaccine_api_test.py
@@ -6,14 +6,16 @@ from tests import ResourceTestCase
 class VaccineResource(ResourceTestCase, unittest.TestCase):
     def __init__(self, method_name='runTest'):
         super().__init__(method_name)
-        self.api_url = '/api/patient/'
+        self.api_url = '/api/vaccine/'
         self.model = Vaccine
         self.data = dict(
-            name='测试狗',
-            description='用来测试的测试狗'
+            name='一个药',
+            price=60,
+            count=50
         )
         self.updated = dict(
-            name='软件狗',
-            description='用来测试的软件狗'
+            name='一个假药',
+            price=61,
+            count=52
         )
-        self.fields = ['name', 'description']
+        self.fields = ['name', 'price', 'count']
